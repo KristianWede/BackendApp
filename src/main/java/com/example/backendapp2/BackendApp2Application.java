7a17ff2278e4c6bb58232c29ca7e62695cdaa8c4
KristianWede <kris79s4@stud.kea.dk>
2023-02-17 13:52:15 +0100

    First Commit

---
+++ src/main/java/com/example/backendapp2/BackendApp2Application.java
@@ -0,0 +1,13 @@
+package com.example.backendapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendapp2/BackendApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
